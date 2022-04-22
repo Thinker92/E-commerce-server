d07f44956ceed30cafaf25d6379def274ff25b23
Vilson Apostolovski II <vilsonapo@gmail.com>
2022-04-22 04:21:11 -0400

    Built associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_name'
+});
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_name'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'tag_name'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  foreignKey: 'product_name'
+})
 
 module.exports = {
   Product,
