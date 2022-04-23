9280180d05fce1912aa955be8ad4d39f5fdc72a2
Vilson Apostolovski II <vilsonapo@gmail.com>
2022-04-23 00:00:48 -0400

    Fixed issues in models and building more routes

--- models/index.js
+++ models/index.js
@@ -6,21 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category,{
-  foreignKey: 'category_name'
+  foreignKey: 'category_id'
 });
 // Categories have many Products
 Category.hasMany(Product,{
-  foreignKey: 'category_name'
+  foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: ProductTag,
-  foreignKey: 'tag_name'
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: ProductTag,
-  foreignKey: 'product_name'
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
